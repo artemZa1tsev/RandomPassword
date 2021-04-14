119986877bd549fc23a75be9f9fb071dac64d097
artemZa1tsev <78979721+artemZa1tsev@users.noreply.github.com>
2021-04-15 00:15:43 +0300

    Add files via upload

---
+++ add.js
@@ -0,0 +1,33 @@
+import RandomGenerate from "./script.js";
+
+
+document.querySelector('.checkboxes').addEventListener('click', function() {
+    RandomGenerate.lowUpperCase();
+});
+
+
+document.querySelector('.checkboxes1').addEventListener('click', function() {
+    RandomGenerate.lowUpperCase();
+});
+
+
+document.getElementById('btn-copy').addEventListener('click', function() {
+     RandomGenerate.textCopy();
+}); 
+
+document.getElementById('customRange1').addEventListener('input', function() {
+    RandomGenerate.passwordLength();
+    RandomGenerate.lowUpperCase();
+
+}); 
+
+document.getElementById('rangeOut').addEventListener('input', function(e) {
+    
+    RandomGenerate.passwordLength();
+    RandomGenerate.lowUpperCase();
+    
+}); 
+
+
+
+
